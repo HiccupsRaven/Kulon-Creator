75ce632fbd243269ea894397b7c7b7ac640ba96a
devanka761 <devanka@protonmail.com>
2026-07-14 13:47:01 +0700

    feat: add landing page

--- src/public.entries.ts
+++ src/public.entries.ts
@@ -1,6 +1,6 @@
 const publicEntries = {
   testplay: "./src/scripts/game.ts",
-  // built: "./src/scripts/built.ts",
+  index: "./src/scripts/home.ts",
   creator: "./src/scripts/creator.ts",
   code: "./src/scripts/code.ts"
 }
---
+++ src/scripts/home.ts
@@ -0,0 +1,4 @@
+import "webfont-awesome-pro/scss/fontawesome.scss"
+import "webfont-awesome-pro/scss/brands.scss"
+import "webfont-awesome-pro/scss/pixel-regular.scss"
+import "../styles/home.scss"
